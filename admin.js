4eff69fd2e9e84ca3ddfe5900477760e47480fee
Diego Sabillon <diegosabillon1@gmail.com>
2024-04-30 02:14:36 -0600

    Update admin.js

--- admin.js
+++ admin.js
@@ -154,7 +154,7 @@ const labels = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
 const data = {
   labels: labels,
   datasets: [{
-    label: 'Usuarios activos',
+    label: 'conexiones activos',
     data: [65, 59, 80, 81, 56, 55, 40],
     backgroundColor: [
       'rgba(255, 99, 132, 0.2)',
